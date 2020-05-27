ca12a25fea8083ea2b0689c42f2302a322e9b513
ycarof <so426h2o>
2020-05-27 11:26:37 +0200

    Add publisher relationship + entity creation refactoring.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -27,37 +27,56 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Author terryP = new Author("Terry", "Pratchett");
-        Book laddm1 = new Book("La huitième couleur", "978-2-84172-689-9");
-        terryP.getBooks().add(laddm1);
-        laddm1.getAuthors().add(terryP);
-
-        Book laddm2 = new Book("Le huitième sortilège", "978-2-84172-690-5");
-        terryP.getBooks().add(laddm2);
-        laddm2.getAuthors().add(terryP);
-
-        authorRepository.save(terryP);
-        bookRepository.save(laddm1);
-        bookRepository.save(laddm2);
-
-        Author bd = new Author("Bernard", "Deschamps");
-        Author jcd = new Author("Jean-Claude", "Deschaintre");
-        Book lldp = new Book("Le livre du pâtissier", "978-2-86268-417-8");
-        bd.getBooks().add(lldp);
-        jcd.getBooks().add(lldp);
-        lldp.getAuthors().add(bd);
-        lldp.getAuthors().add(jcd);
-
-        authorRepository.save(bd);
-        authorRepository.save(jcd);
-        bookRepository.save(lldp);
-
         System.out.println("Started in Bootstrap");
-        System.out.println("Number of saved book : " + bookRepository.count());
 
-        Publisher atalante = new Publisher("Librairie L'Atalante", "15, rue des Vieilles-Douves", "Nantes", "France", "44000");
-        publisherRepository.save(atalante);
+        Publisher atalantePublisher = new Publisher("Librairie L'Atalante", "15, rue des Vieilles-Douves", "Nantes", "France", "44000");
+        publisherRepository.save(atalantePublisher);
+        Publisher ltJLPublisher = new Publisher("Editions LT Jacques Lanore", "5, allée de la 2e D.B.", "Paris", "France", "75015");
+        publisherRepository.save(ltJLPublisher);
+
+        Author terryPAuthor = new Author("Terry", "Pratchett");
+        authorRepository.save(terryPAuthor);
+
+        Book laddm1Book = new Book("La huitième couleur", "978-2-84172-689-9");
+        bookRepository.save(laddm1Book);
+
+        terryPAuthor.getBooks().add(laddm1Book);
+        laddm1Book.getAuthors().add(terryPAuthor);
+        laddm1Book.setPublisher(atalantePublisher);
+        atalantePublisher.getBooks().add(laddm1Book);
+        authorRepository.save(terryPAuthor);
+        bookRepository.save(laddm1Book);
+        publisherRepository.save(atalantePublisher);
+
+        Book laddm2Book = new Book("Le huitième sortilège", "978-2-84172-690-5");
+        terryPAuthor.getBooks().add(laddm2Book);
+        laddm2Book.getAuthors().add(terryPAuthor);
+        laddm2Book.setPublisher(atalantePublisher);
+        atalantePublisher.getBooks().add(laddm2Book);
+        authorRepository.save(terryPAuthor);
+        bookRepository.save(laddm2Book);
+        publisherRepository.save(atalantePublisher);
+
+        Author bdAuthor = new Author("Bernard", "Deschamps");
+        Author jcdAuthor = new Author("Jean-Claude", "Deschaintre");
+        Book lldpBook = new Book("Le livre du pâtissier", "978-2-86268-417-8");
+        bdAuthor.getBooks().add(lldpBook);
+        jcdAuthor.getBooks().add(lldpBook);
+        lldpBook.getAuthors().add(bdAuthor);
+        lldpBook.getAuthors().add(jcdAuthor);
+        lldpBook.setPublisher(ltJLPublisher);
+        ltJLPublisher.getBooks().add(lldpBook);
+
+        authorRepository.save(bdAuthor);
+        authorRepository.save(jcdAuthor);
+        bookRepository.save(lldpBook);
+        publisherRepository.save(ltJLPublisher);
+
         System.out.println("Number of saved publisher : " + publisherRepository.count());
+        System.out.println("Number of saved author : " + authorRepository.count());
+        System.out.println("Number of saved book : " + bookRepository.count());
+        System.out.println("Number of saved book(s) for Atalante publisher : " + atalantePublisher.getBooks().size());
+        System.out.println("Number of saved book(s) for LT publisher : " + ltJLPublisher.getBooks().size());
 
     }
 }
